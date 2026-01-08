e63869e65fdd658fd81b71a9ae883e5f71e79d98
cosmosayo <alfredchrisayo@gmail.com>
2026-01-08 05:14:41 +0100

    stageing

--- MT.cs
+++ MT.cs
@@ -1,23 +1,11 @@
 using System;
-namespace MultiplicationTable
+namespace Dicey
 {
-    class Engine
-    {
+ program DiceyEngine
+    {}
         static void Main(string[] args)
-        {
-            Console.Clear();
-            Console.Write("Which table would you like to count: ");
-            int table = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("How many table would you like to see multiplication for: ");
-            int count = Convert.ToInt32(Console.ReadLine());
-
-            for (int i = 1; i <= count; i++)
-            {
-                double result = table * i;
-                Console.WriteLine(result);
-            }
-            Console.ReadKey();
-        }
+    {
+        
     }
+    
 }
